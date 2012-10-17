1d095b51ddcafd61ff73ce2e83f07830d5569e1d
Mauricio Dominguez <mauriciod73@gmail.com>
2012-10-16 19:19:48 -0500

    Commit Inicial

---
+++ Lab01_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab01_1.MvcApplication" Language="C#" %>
---
+++ Lab01_1/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01_1.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
